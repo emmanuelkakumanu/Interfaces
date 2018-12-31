d9ea17e93b237fe54e0af43ae582e9eaeddd4db0
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-12-31 11:07:03 +0530

    Create 7.java

---
+++ 7.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+interface in1
+{
+	void method1();
+}
+interface in2 extends in1
+{}
+class Ideone implements in2
+{
+	public void method1()
+	{
+		System.out.println("method1() implemented");
+	}
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Ideone obj=new Ideone();
+		obj.method1();
+	}
+}
