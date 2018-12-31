1d8c64946e0e247f485cb3e85bca096de2c5ef8a
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-12-31 11:16:11 +0530

    Create 11.java

---
+++ 11.java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+interface in1
+{
+	static final int a=10;
+	void method1();
+}
+class Ideone implements in1
+{
+	public void method1()
+	{
+		System.out.println("method1() implemented"+"\n a : "+a);
+		
+	}
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Ideone obj=new Ideone();
+		obj.method1();
+		System.out.println("a="+a);
+		System.out.println("a="+obj.a);
+	}
+}
